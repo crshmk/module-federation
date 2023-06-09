44a4b544e90fb0f862e9c85c1b9197045f1c8286
Chris Hammock <chris@crshmk.com>
2023-06-09 09:37:19 +0700

    remote2: add providers for shared state

--- src/remote2/src/App.js
+++ src/remote2/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import List from './List'
+import { CountProvider } from 'host/useCount'
 
 const App = () => (
+  <CountProvider>
   <div>
     <p>remote 2 development container</p>
     <List />
   </div>
+  </CountProvider>
 )
 
 export default App
