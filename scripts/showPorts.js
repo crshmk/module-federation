ff822b774cfc8bc0eb676cb81a4216ce267016a1
Chris Hammock <chris@crshmk.com>
2023-06-19 10:32:24 +0700

    simplify show-ports script

--- scripts/showPorts.js
+++ scripts/showPorts.js
@@ -6,24 +6,13 @@
 require('dotenv').config()
 const modules = require('../moduleNames.json')
 
-const { addIndex, pipe, reduce, replace } = require('ramda')
-
 const { hostPort } = process.env
-const reduceIndexed = addIndex(reduce)
-
-const removeLastComma = str => 
-  str.replace(/,([^,]*)$/, '$1')
 
 const appendPort = (acc, moduleName, i) => {
   const port = +hostPort + i
-  return acc + `${moduleName}: ${port}, \n`
+  return acc + `${moduleName}: ${port} \n`
 }
 
-const makePorts = pipe(
-  reduceIndexed(appendPort, ''),
-  removeLastComma
-)
-
-const ports = makePorts(modules)
+const ports = modules.reduce(appendPort, '') 
 
 console.log(ports)
